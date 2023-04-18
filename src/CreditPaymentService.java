8a1cd1881a997906eb2362e0197d45e13b75dce3
Misha <bilan.md72@gmail.com>
2023-04-18 23:25:22 +0500

    change

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public double calculate(int credit, double percent, int time) {
-        double month = percent / time / 100;
+        double month = percent / 12 / 100;
         double result = credit * (month * (Math.pow(1 + month, time)) / (Math.pow(1 + month, time) - 1));
         return result;
     }
